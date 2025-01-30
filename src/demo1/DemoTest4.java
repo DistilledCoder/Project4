0ec55e817265cf297951fcbd5165a3e855c4d9fa
ericw <ericw@UncleRicco.san.rr.com>
2025-01-29 18:05:35 -0800

    added an int variable and a print function

--- src/demo1/DemoTest4.java
+++ src/demo1/DemoTest4.java
@@ -3,5 +3,11 @@ package demo1;
 public class DemoTest4 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		
+		int q = 89;
+		
+		System.out.println("Eric is learning");
+		System.out.println(q);
 }
 }
